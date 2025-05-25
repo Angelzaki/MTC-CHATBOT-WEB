43a497580701518f3906d58a0428b74ca3721475
Angel David (Zaki) <140128799+Angelzaki@users.noreply.github.com>
2025-05-24 22:27:13 -0500

    Update firebaseconfig.js

--- frontendversionweb/src/firebaseconfig.js
+++ frontendversionweb/src/firebaseconfig.js
@@ -4,13 +4,13 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCP1qGEC32JdAdaQ3pk3xgNq32vYXPqB70",
-  authDomain: "chatderm-a65c0.firebaseapp.com",
-  projectId: "chatderm-a65c0",
-  storageBucket: "chatderm-a65c0.firebasestorage.app",
-  messagingSenderId: "1038720370031",
-  appId: "1:1038720370031:web:c8769db7517ab15da8549a",
-  measurementId: "G-0PKKSCF9BJ"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 const app = initializeApp(firebaseConfig);
